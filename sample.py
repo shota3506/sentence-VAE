c35c2efd1ffd1e6c70f20582a1d2a5b37b98ad47
sugiura <s.shota.710.3506@gmail.com>
2021-02-13 13:01:18 +0900

    Add sampling

--- sample.py
+++ sample.py
@@ -24,6 +24,7 @@ parser.add_argument('--num_layers', type=int, default=2)
 parser.add_argument('--bidirectional', action='store_true')
 parser.add_argument("--checkpoint_file", type=str, default="model.pth")
 # Search
+parser.add_argument("--sample_size", type=int, default=10)
 parser.add_argument("--search_width", type=int, default=1)
 
 args = parser.parse_args()
@@ -48,36 +49,18 @@ def main() -> None:
     model.load_state_dict(torch.load(args.checkpoint_file, map_location=device))
     model.eval()
 
-    sentence1 = input('Please input sentence1: ')
-    sentence2 = input('Please input sentence2: ')
+    z = torch.randn(args.sample_size, args.dim_latent, device=device)
+    hidden = model.fc_hidden(z)
+    hidden = hidden.view(args.sample_size, -1, model.dim_hidden).transpose(0, 1).contiguous()
 
-    s1 = [tokenizer.bos_index] + tokenizer.encode(sentence1) + [tokenizer.eos_index]
-    s2 = [tokenizer.bos_index] + tokenizer.encode(sentence2) + [tokenizer.eos_index]    
+    start_predictions = torch.zeros(args.sample_size, device=device).fill_(tokenizer.bos_index).long()
+    start_state = {'hidden': hidden.permute(1, 0, 2)}
+    predictions, log_probabilities = searcher.search(start_predictions, start_state, model.step)
 
-    z1, _ = model.encode(
-        torch.tensor([s1]).to(device),
-        torch.tensor([len(s1)]).to(device)
-    )
-    z2, _ = model.encode(
-        torch.tensor([s2]).to(device),
-        torch.tensor([len(s2)]).to(device)
-    )
-
-    print("\nGenerate intermediate sentences")
-    print("      %s" % sentence1)
-    for r in range(1, 10):
-        z = (1 - 0.1 * r) * z1 + 0.1 * r * z2 
-        hidden = model.fc_hidden(z)
-        hidden = hidden.view(1, -1, model.dim_hidden).transpose(0, 1).contiguous()
-
-        start_predictions = torch.zeros(1, device=device).fill_(tokenizer.bos_index).long()
-        start_state = {'hidden': hidden.permute(1, 0, 2)}
-        predictions, log_probabilities = searcher.search(start_predictions, start_state, model.step)
-
-        tokens = predictions[0, 0]
+    for pred in predictions:
+        tokens = pred[0]
         tokens = tokens[tokens != tokenizer.eos_index].tolist()
-        print("[%d:%d] %s" % (10 - r, r, tokenizer.decode(tokens)))
-    print("      %s" % sentence2)
+        print(tokenizer.decode(tokens))
 
 if __name__ == "__main__":
     main()
